a208cdbd0a60afe3ff6fbfb094b1656067a01910
Sophie Lee <44243849+gahyunlee@users.noreply.github.com>
2020-02-11 23:15:24 -0600

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -39,3 +39,51 @@ if __name__ == '__main__':
         print('Weird')
     else:
         print('Not Weird')
+
+        
+'''
+Task
+Read two integers from STDIN and print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+
+Input Format
+The first line contains the first integer, a. The second line contains the second integer, b.
+
+Constraints
+1<= a <= 10**10
+1<= b <= 10**10
+
+Output Format
+Print the three lines as explained above.
+'''
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    
+    
+'''
+Task
+Read two integers and print two lines. The first line should contain integer division, a//b. 
+The second line should contain float division, a/b.
+
+You don't need to perform any rounding or formatting operations.
+
+Input Format
+The first line contains the first integer, . The second line contains the second integer, .
+
+Output Format
+Print the two lines as described above.
+'''
+
+a = int(input())
+b = int(input())
+print(a//b)
+print(a/b)
